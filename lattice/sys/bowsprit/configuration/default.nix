529c7b6e5a8f0ecc575e2484ef351324cd760fd2
lattice <315987+elmarsto@users.noreply.github.com>
2024-01-28 01:25:30 -0800

    tweak bowsprit ok

--- lattice/sys/bowsprit/configuration/default.nix
+++ lattice/sys/bowsprit/configuration/default.nix
@@ -2,37 +2,35 @@
   credential = builtins.readFile ./secret.fido2luks.id;
 in {
   system.stateVersion = "22.05"; # Did you read the comment?
-  config = {
-      boot = {
-        loader = {
-            systemd-boot.enable = true;
-            efi.canTouchEfiVariables = true;
-        };
-        kernel.sysctl = {
-          "kernel.sysrq" = 0; # safer for laptop to disable sysrq
-        };
-        kernelParams = [
-          "console=ttyS0,115200"
-          "console=tty1"
-        ];
+    boot = {
+      loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
       };
-      programs.xwayland.enable = true;
-      services.xserver = {
-        enable = true;
-        displayManager = {
-          defaultSession = "gnome";
-          gdm = {
-            enable = true;
-            wayland = true;
-          };
+      kernel.sysctl = {
+        "kernel.sysrq" = 0; # safer for laptop to disable sysrq
+      };
+      kernelParams = [
+        "console=ttyS0,115200"
+        "console=tty1"
+      ];
+    };
+    programs.xwayland.enable = true;
+    services.xserver = {
+      enable = true;
+      displayManager = {
+        defaultSession = "gnome";
+        gdm = {
+          enable = true;
+          wayland = true;
         };
-        desktopManager.gnome.enable = true;
       };
-      hardware = {
-        cpu.intel.updateMicrocode = true;
-        enableRedistributableFirmware = lib.mkDefault true;
-      };
-  };
+      desktopManager.gnome.enable = true;
+    };
+    hardware = {
+      cpu.intel.updateMicrocode = true;
+      enableRedistributableFirmware = lib.mkDefault true;
+    };
   imports =
     [
       ./network.nix
