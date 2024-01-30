6bb112642b9d829046ea609caeb72bf9b845b185
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-01-29 16:18:16 -0800

    udev

--- lattice/sys/bowsprit/configuration/default.nix
+++ lattice/sys/bowsprit/configuration/default.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, ... }: {
-  imports = [ ./hardware-configuration.nix ];
   networking = {
     hostName = "bowsprit";
     hostId = "a0300937"; # first 8 of /etc/machine-id, req for zfs
     networkmanager.enable = true;
     interfaces.wlp0s20f3.useDHCP = true;
   };
-  udev.extraRules = ''
+  imports = [ ./hardware-configuration.nix ];
+  services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
   '';
 }
