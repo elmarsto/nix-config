52b13c3b5ad7a08e4703f1a16a7013c7b0653381
lattice <315987+elmarsto@users.noreply.github.com>
2024-02-02 15:54:50 -0800

    fix

--- lattice/sys/sopwith/home.nix
+++ lattice/sys/sopwith/home.nix
@@ -1,16 +1,1 @@
-{ lib, config, lattice, ... }:  {
-  config = {
-    home = {
-      username = "lattice";
-      homeDirectory = "/home/lattice";
-    };
-    lattice = {
-      spool = "/var/mail/lattice";
-      gui.enable = true;
-      vcs = {
-        email = "315987+elmarsto@users.noreply.github.com";
-        name = config.home.username;
-      };
-    };
-  };
-}
+{ ... }:  { }
