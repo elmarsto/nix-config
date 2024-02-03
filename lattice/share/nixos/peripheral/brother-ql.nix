8ce2e1f82775e4a79c64c4dbb7ccd67c899331e7
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-02-03 13:07:42 -0800

    cleanup

--- lattice/share/nixos/peripheral/brother-ql.nix
+++ lattice/share/nixos/peripheral/brother-ql.nix
@@ -1,6 +1,12 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: let
+  brother-ql = inputs.brother-ql.packages.x86_64-linux.default;
+  #RESEARCH: doas rules for only sending to the given USB and nowhere lese
+  bql = pkgs.writeScriptBin "bql" ''
+   ${brother-ql}/bin/brother_ql -b pyusb -m QL-800 -p usb://0x04f9:0x209b print  -l 62 $@
+   '';
+in {
   environment.systemPackages = [
-    # FIXME: hardwired architecture
-    inputs.brother-ql.packages.x86_64-linux.default
+    brother-ql
+    bql
   ];
 }
