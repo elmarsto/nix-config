de19d7784753cacd4ff4eb1712ab75e9278eb548
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-02-03 13:55:27 -0800

    brotherql

--- lattice/share/nixos/peripheral/brother-ql.nix
+++ lattice/share/nixos/peripheral/brother-ql.nix
@@ -1,9 +1,16 @@
-{ inputs, pkgs, ... }: let
+{ pkgs, ... }: let
   bql = pkgs.writeScriptBin "bql" ''
-   nix run github:elmarsto/brother-ql -b pyusb -m QL-800 -p usb://0x04f9:0x209b print -l 62 $@
+    nix run github:elmarsto/brother-ql -m QL-800 -b pyusb -p usb://0x04f9:0x209b $@
    '';
+  bql-p = pkgs.writeScriptBin "bql" ''
+    ${bql} print -l 62 $@
+   '';
+  doasRule = cmd: {
+    cmd = "${cmd}";
+    groups = [ "wheel" ];
+    noPass = true;
+  };
 in {
-  environment.systemPackages = [
-    bql
-  ];
+  environment.systemPackages = [ bql bql-p ];
+  security.doas.extraRules = builtins.map doasRule [bql bql-p];
 }
