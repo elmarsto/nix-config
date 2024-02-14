123cd79b83238cb678c5b26bdbe524bc61b357b7
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-02-14 10:40:02 -0800

    feat: printers

--- lattice/share/nixos/peripheral/default.nix
+++ lattice/share/nixos/peripheral/default.nix
@@ -1,14 +1,22 @@
 { config, lib, pkgs, ... }: {
   imports = [
     ./brother-ql.nix
-    ./epson.nix
     ./rng.nix
     ./wacom.nix
     ./xbox.nix
     ./yubikey.nix
   ];
   services.printing = {
-     enable = true;
-     drivers = [ pkgs.gutenprint pkgs.gutenprintBin ];
+    enable = true;
+    drivers = with pkgs; [ gutenprint gutenprintBin espon-escpr hplip canon-cups-ufr2 ];
+    browsing = true;
+    allowFrom = ["all"];
+    defaultShared = true;
+  };
+  hardware.sane = {
+      openFirewall = true;
+      enable = true;
+      extraBackends = with pkgs; [ utsushi hplipWithPlugin sane-airscan ];
+      brscan5.enable = true;
   };
 }
