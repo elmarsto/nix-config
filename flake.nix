8d21a3a9705e55f3144320f477f90444bee170f8
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-03-05 13:37:35 -0800

    unstable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
