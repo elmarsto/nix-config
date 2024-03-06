8c85f2b25654cda1509f4f227ea5c300e8659fcc
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-03-06 12:44:18 -0800

    feat: downgrade to docker from podman for act

--- lattice/share/home-manager/virt.nix
+++ lattice/share/home-manager/virt.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: {
   home.packages = with pkgs; [
-    podman-compose
+    docker-compose
     lazydocker
     runc
     virt-manager
--- lattice/share/nixos/virt.nix
+++ lattice/share/nixos/virt.nix
@@ -7,20 +7,22 @@
     kernelModules = ["kvm" "kvm-amd" "kvm-intel" "vfio-pci"];
     kernel.sysctl = {"vm.hugetlb_shm_group" = "301";};
   };
-  environment.systemPackages = with pkgs; [virt-manager podman];
+  environment.systemPackages = with pkgs; [virt-manager docker];
   programs.dconf.enable = true; #needed by virt-manager
   services.flatpak.enable = true;
   virtualisation = {
     libvirtd.enable = true;
-    docker.enable = false;
+    docker.enable = true;
     containers.cdi.dynamic.nvidia.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      dockerSocket.enable = true;
-      defaultNetwork.settings.dns_enabled = true;
-      defaultNetwork.settings.dnsname_enabled = true;
-    };
+    # podman = {
+    #   enable = true;
+    #   dockerCompat = true;
+    #   dockerSocket.enable = true;
+    #   defaultNetwork.settings =  {
+    #     dns_enabled = true;
+    #     dnsname_enabled = true;
+    #   };
+    # };
     spiceUSBRedirection.enable = true;
   };
   xdg.portal.enable = true;
