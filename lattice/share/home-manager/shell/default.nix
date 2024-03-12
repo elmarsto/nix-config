5486f9cd3fcd54de8631df084359830e56be5bcf
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-03-12 16:13:44 -0700

    eza.enableAliases

--- lattice/share/home-manager/shell/default.nix
+++ lattice/share/home-manager/shell/default.nix
@@ -158,7 +158,6 @@ in {
     broot.enable = true;
     eza = {
       enable = true;
-      enableAliases = true;
       extraOptions = ["--group-directories-first" "--header"];
       git = true;
       icons = true;
