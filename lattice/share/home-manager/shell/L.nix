25ebc5bdce6d52ff57bd4b729aa66e1921adca69
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-03-13 17:16:01 -0700

    blast

--- lattice/share/home-manager/shell/L.nix
+++ lattice/share/home-manager/shell/L.nix
@@ -7,14 +7,30 @@
   gitJust = pkgs.writeText "gitJust" ''
     [no-cd]
     default:
-      ${pkgs.git}/bin/git status
+     git status
+
+    [no-cd]
+    blast msg='blast':
+      git add .
+      git commit -m '{{ msg }}'
+      git pull -r
+      git push
   '';
   # sorry not sorry
   Lustfile = pkgs.writeText "Lustfile" ''
     mod? l '~/.l/mod.just'
     mod git '${gitJust}'
     default:
-      echo `pwd`
+      #!/usr/bin/env bash
+      echo <<'EOF'
+       ___
+      |\  \
+      \ \  \
+       \ \  \
+        \ \  \____
+         \ \_______\
+          \|_______|
+      EOF
   '';
   L = pkgs.writeScriptBin "L" ''
     ${pkgs.just}/bin/just --unstable -d ~ -f ${Lustfile} "$@"
@@ -22,11 +38,18 @@
   git-L = pkgs.writeScriptBin "git-L" ''
     ${L}/bin/L git "$@"
   '';
+  l = pkgs.writeScriptBin "l" ''
+    ${L}/bin/L l "$@"
+  '';
+  git-l = pkgs.writeScriptBin "git-l" ''
+    ${l}/bin/l git "$@"
+  '';
 in {
   home = {
-    packages = [L git-L];
+    packages = [L l git-L git-l];
   };
   programs.bash.initExtra = ''
     export JUST_UNSAFE=1
+    unalias l
   '';
 }
