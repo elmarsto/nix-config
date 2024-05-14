ee9796c4b1ee70d244c7d83876ddd00d8e1c0f2d
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-05-13 20:29:42 -0700

    mona

--- lattice/share/home-manager/gui/wezterm.nix
+++ lattice/share/home-manager/gui/wezterm.nix
@@ -1,71 +1,72 @@
-{ config, pkgs, lib, ... }: { home = {
-  file.".wezterm.lua".source = pkgs.writeText "wezterm" ''
-    -- Pull in the wezterm API
-    local wezterm = require 'wezterm'
+{pkgs, ...}: {
+  home = {
+    file.".wezterm.lua".source = pkgs.writeText "wezterm" ''
+      -- Pull in the wezterm API
+      local wezterm = require 'wezterm'
 
-    -- This table will hold the configuration.
-    local c = {}
+      -- This table will hold the configuration.
+      local c = {}
 
-    -- In newer versions of wezterm, use the config_builder which will
-    -- help provide clearer error messages
-    if wezterm.config_builder then
-      c = wezterm.config_builder()
-    end
+      -- In newer versions of wezterm, use the config_builder which will
+      -- help provide clearer error messages
+      if wezterm.config_builder then
+        c = wezterm.config_builder()
+      end
 
-    -- This is where you actually apply your config choices
+      -- This is where you actually apply your config choices
 
-    -- For example, changing the color scheme:
-    c.color_scheme = 'Tomorrow Night'
-    c.xcursor_theme = 'Adwaita'
-    c.font = wezterm.font 'Monaspace Argon'
-    c.harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" }
-    c.font_size = 14
-    c.font_rules = {
-      {
-        underline = 'Single',
-        font = wezterm.font { family = 'Monaspace Krypton', weight = 'Bold' }
-      },
-      {
-        intensity = 'Half',
-        italic = false,
-        font = wezterm.font { family = 'Monaspace Argon', weight = 'ExtraLight', style = 'Normal' }
-      },
-      {
-        intensity = 'Normal',
-        italic = false,
-        font = wezterm.font { family = 'Monaspace Argon', weight = 'Regular', style = 'Normal' }
-      },
-      {
-        intensity = 'Bold',
-        italic = false,
-        font = wezterm.font { family = 'Monaspace Neon', weight = 'ExtraBold', style = 'Normal' }
-      },
-      {
-        intensity = 'Half',
-        italic = true,
-        font = wezterm.font { family = 'Monaspace Radon', weight = 'ExtraLight', style = 'Normal' }
-      },
-      {
-        intensity = 'Normal',
-        italic = true,
-        font = wezterm.font { family = 'Monaspace Radon', weight = 'Regular', style = 'Normal' }
-      },
-      {
-        intensity = 'Bold',
-        italic = true,
-        font = wezterm.font { family = 'Monaspace Neon', weight = 'ExtraBold', style = 'Italic' }
-      },
-    }
-    c.underline_position = -5
-    c.colors = { background = 'rgba(50, 50, 50, 0.8)' }
-    c.hide_tab_bar_if_only_one_tab = true
-    c.tab_bar_at_bottom = true
-    c.use_fancy_tab_bar = false
-    c.warn_about_missing_glyphs = false
+      -- For example, changing the color scheme:
+      c.color_scheme = 'Tomorrow Night'
+      c.xcursor_theme = 'Adwaita'
+      c.font = wezterm.font 'Monaspace Argon'
+      c.harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "liga" }
+      c.font_size = 14
+      c.font_rules = {
+        {
+          underline = 'Single',
+          font = wezterm.font { family = 'Monaspace Krypton', weight = 'Bold' }
+        },
+        {
+          intensity = 'Half',
+          italic = false,
+          font = wezterm.font { family = 'Monaspace Argon', weight = 'ExtraLight', style = 'Normal' }
+        },
+        {
+          intensity = 'Normal',
+          italic = false,
+          font = wezterm.font { family = 'Monaspace Argon', weight = 'Regular', style = 'Normal' }
+        },
+        {
+          intensity = 'Bold',
+          italic = false,
+          font = wezterm.font { family = 'Monaspace Neon', weight = 'ExtraBold', style = 'Normal' }
+        },
+        {
+          intensity = 'Half',
+          italic = true,
+          font = wezterm.font { family = 'Monaspace Radon', weight = 'ExtraLight', style = 'Normal' }
+        },
+        {
+          intensity = 'Normal',
+          italic = true,
+          font = wezterm.font { family = 'Monaspace Radon', weight = 'Regular', style = 'Normal' }
+        },
+        {
+          intensity = 'Bold',
+          italic = true,
+          font = wezterm.font { family = 'Monaspace Neon', weight = 'ExtraBold', style = 'Italic' }
+        },
+      }
+      c.underline_position = -5
+      c.colors = { background = 'rgba(50, 50, 50, 0.8)' }
+      c.hide_tab_bar_if_only_one_tab = true
+      c.tab_bar_at_bottom = true
+      c.use_fancy_tab_bar = false
+      c.warn_about_missing_glyphs = false
 
-    -- and finally, return the configuration to wezterm
-    return c
-  '';
-  packages = [ pkgs.wezterm ];
-};}
-
+      -- and finally, return the configuration to wezterm
+      return c
+    '';
+    packages = [pkgs.wezterm];
+  };
+}
