b28775b42a19160c80a87d97d615a8516c0326ef
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-07-26 13:28:29 -0700

    feat: reaper encore

--- lattice/share/home-manager/audio.nix
+++ lattice/share/home-manager/audio.nix
@@ -1,20 +1,26 @@
-{ config, pkgs, lib,  ... }: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [
     castget
     mpv
     sayonara
     spotify
+    reaper
   ];
   xdg = {
     mimeApps = {
       defaultApplications = {
-        "audio/mpeg" = [ "mpv.desktop" ];
-        "audio/wav" = [ "mpv.desktop" ];
+        "audio/mpeg" = ["mpv.desktop"];
+        "audio/wav" = ["mpv.desktop"];
       };
     };
     desktopEntries = {
       mpv = {
-        mimeType = [ "audio/mpeg" "audio/wav" ];
+        mimeType = ["audio/mpeg" "audio/wav"];
       };
     };
   };
