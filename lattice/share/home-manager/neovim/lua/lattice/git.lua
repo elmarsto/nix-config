e609667457213824b95c033dae8fc4eb065d25be
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2024-10-07 12:05:11 -0700

    fix: last fix

--- lattice/share/home-manager/neovim/lua/lattice/git.lua
+++ lattice/share/home-manager/neovim/lua/lattice/git.lua
@@ -1,7 +1,7 @@
 local git = {}
 
 function git.setup()
-  require('git-conflict').setup({})
+  -- require('git-conflict').setup({}) -- disabled 2024 Oct 07, was causing conflicts
   vim.g.blamer_enabled = true
   require("gitsigns").setup()
 end
