28edd8a385b8917b54117705894348e78c4eddbf
Elizabeth Marston <elizabeth.marston@quandri.io>
2024-11-13 14:59:45 -0800

    fix: pamplemoose

--- lattice/sys/pamplemoose/home.nix
+++ lattice/sys/pamplemoose/home.nix
@@ -1,15 +1,14 @@
-{ lattice, ... }:  {
+{lattice, ...}: {
   imports = [
     (lattice + /share/home-manager/neovim)
     (lattice + /share/home-manager/shell)
+    (lattice + /share/home-manager/visual.nix)
     (lattice + /share/home-manager/text.nix)
     (lattice + /share/home-manager/vcs.nix)
   ];
   home = {
     username = "elizabeth.marston";
-    
+
     homeDirectory = "/Users/elizabeth.marston";
   };
-
-
 }
