0dde0fc43f677f5466cb7989ec51411f04f59b30
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2025-07-10 22:31:39 -0700

    feat: simplify

--- lattice/share/home-manager/shell/L.nix
+++ lattice/share/home-manager/shell/L.nix
@@ -1,90 +1,23 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  gitJust = pkgs.writeText "gitJust" ''
-    just := 'L git'
-    code := '~/code'
-    ours := '{{code}}/ours'
-    navaruk := '~/navaruk'
-    nix-config := '{{ ours }}/nix-config'
-
-    [no-cd]
-    default:
-     git status
-
-    [no-cd]
-    blast msg='blast':
-      git add .
-      git commit -m '{{ msg }}'
-      git pull -r
-      git push
-
-    _eod repo:
-      pushd {{ repo }}
-      git pull
-      git add .
-      git commit -am 'eod'
-      git push
-      git co -
-      popd
-
-    eod:
-      {{ just }} _eod  {{ navaruk }}
-      {{ just }} _eod  {{ nix-config }}
-
-  '';
+{pkgs, ...}: let
   # sorry not sorry
   Lustfile = pkgs.writeText "Lustfile" ''
     just := 'L'
     today := `date +%F`
-    mod? l '~/.l/mod.just'
-    mod git '${gitJust}'
+    [private]
     default:
-      #!/usr/bin/env bash
-      echo <<'EOF'
-       ___
-      |\  \
-      \ \  \
-       \ \  \
-        \ \  \____
-         \ \_______\
-          \|_______|
-      EOF
+      L --list
 
-      eod:
-        {{ L }} git eod
-
-
+    svgify:
+      echo "TODO"
   '';
   L = pkgs.writeScriptBin "L" ''
     ${pkgs.just}/bin/just --unstable -d ~ -f ${Lustfile} "$@"
   '';
-  git-L = pkgs.writeScriptBin "git-L" ''
-    ${L}/bin/L git "$@"
-  '';
-  l = pkgs.writeScriptBin "l" ''
-    ${L}/bin/L l "$@"
-  '';
-  git-l = pkgs.writeScriptBin "git-l" ''
-    ${l}/bin/l git "$@"
-  '';
-  git-hub = pkgs.writeScriptBin "git-hub" ''
-    ${pkgs.git}/bin/git clone "git@github.com:$1"
-  '';
 in {
   home = {
-    packages = [L l git-L git-l git-hub];
-    file.l = {
-      recursive = true;
-      source = ./l;
-      target = ".l";
-    };
+    packages = [L];
   };
   programs.bash.initExtra = ''
     export JUST_UNSAFE=1
-    unalias l
   '';
 }
